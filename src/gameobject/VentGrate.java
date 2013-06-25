43ab49cc0cdc3a8612e057b274f341d723690644
Frogging101 <Brooks572@gmail.com>
2013-06-25 10:50:24 -0400

    Add Ant buildfile

--- src/gameobject/VentGrate.java
+++ src/gameobject/VentGrate.java
@@ -4,6 +4,7 @@ public class VentGrate extends GameObject{
 	boolean electrified;
 	
 	public VentGrate(){
+		electrified = true;
 		this.examineText = "It's shiny and made of metal. As you approach it, you hear a faint buzzing sound emanating from it.";
 	}
 }
