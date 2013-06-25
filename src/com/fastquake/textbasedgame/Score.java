bca5e8e40cff8e926cf6ed77be7b80ee6d40ec6c
Frogging101 <Brooks572@gmail.com>
2013-06-25 19:55:11 -0400

    Forgot the Score.java file

---
+++ src/com/fastquake/textbasedgame/Score.java
@@ -0,0 +1,11 @@
+package com.fastquake.textbasedgame;
+
+public class Score {
+	public long time;
+	public int score;
+	
+	public Score(long time, int score){
+		this.time = time;
+		this.score = score;
+	}
+}
