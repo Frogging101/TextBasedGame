34b608a4418b4e19676b9599f8f24492dd7e0da8
Frogging101 <Brooks572@gmail.com>
2013-06-25 14:58:30 -0400

    Removed needless encapsulation on Door's lock, add open field

--- src/com/fastquake/textbasedgame/room/Door.java
+++ src/com/fastquake/textbasedgame/room/Door.java
@@ -6,23 +6,12 @@ import com.fastquake.textbasedgame.room.*;
 
 public class Door {
 	private int targetId;
-	private boolean locked;
+	public boolean locked;
+	public int open;
 	
 	Door(int targetId, boolean locked){
 		this.targetId = targetId;
 		this.locked = true;
-	}
-	
-	public void lock(){
-		this.locked = true;
-	}
-	
-	public void unlock(){
-		this.locked = false;
-	}
-	
-	public boolean isLocked(){
-		return this.locked;
 	}
 	
 	public int getTargetId(){
